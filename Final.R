bc8b9fefd7540c956aae071383cb8dee6910eb1c
cmend006 <120422813+cmend006@users.noreply.github.com>
2022-12-12 19:59:41 -0500

    Update Final.R

--- Final.R
+++ Final.R
@@ -47,6 +47,12 @@ index = sort(sample(nrow(scaled_df), nrow(scaled_df) * 0.4))
 
 train = scaled_df[index,]
 valid = scaled_df[-index,]
+## Method for undersampling
+
+table(train$output)
+undersampling<-ovun.sample(output~., data=train, method='under', N = 110)$data
+
+### Method for oversampling
 
 # Logistic Regression
 
@@ -82,6 +88,38 @@ df$Predicted = as.factor(df$Predicted)
 
 confusionMatrix(df$Predicted, df$Actual, mode = "everything")
 
+## Logistic Regression- Undersample
+under_log<- glm(output~.,data=undersampling, family='binomial')
+summary(under_log)
+
+##Predicting results
+predict_under<-predict(under_log, newdata= valid, type='response')
+predict_under
+
+##Converting them to alive and dead 
+predict_under_response<-ifelse(predict_under>=0.5, '1','0')
+
+##Dataframe
+dataframe_under<- data.frame('Actual'=valid$output,
+                             'Predicted'= predict_under_response)
+
+View(dataframe_under)
+
+##Creation of confusion matrix
+table_under<-table('Actual'=dataframe_under$Actual,
+                   'Predicted'= dataframe_under$Predicted)
+table_under
+
+##Creation of probability table
+prop_table_under<-prop.table(table_under)
+round(prop_table_under*100,2)
+acc_under<-45.60+34.62
+acc_under
+## 80.22% Accuracy for Undersampling
+##Psuedo r squared
+psuedo_under<-1-(under_log$deviance/under_log$null.deviance)
+psuedo_under
+
 # Classification Trees
 model = rpart(output~., data = train, method = 'class')
 
@@ -106,12 +144,44 @@ acc2
 confusion_matrix = table(valid$output, predict_test)
 confusion_matrix
 
+##Classification- Undersample
+modunder = rpart(output~., data = undersampling, method = 'class')
+
+rpart.plot(modunder)
+
+predict_classunder = predict(modunder, valid, type = "class")
+
+## Dataframe 
+under_class_df = data.frame("Actual" = valid$output,
+                            "Predicted" = predict_classunder)
+View(predict_classunder)
+
+tb_class_under = table("Actual" = under_class_df$Actual,
+                 "Predicted" = under_class_df$Predicted)
+tb_class_under
+
+prob_class_under = round(prop.table(tb_class_under) * 100, 2)
+prob_class_under
+
+accuracy_under_class = 31.87+39.56
+accuracy_under_class
+
+confusion_matrix = table(valid$output, predict_classunder)
+confusion_matrix
+
 # Random Forests
 rf = randomForest(output~., data = train, proximity=TRUE) 
 print(rf)
 
 p1 = predict(rf, train)
 confusionMatrix(p1, train$output)
+
+## Random Forest- Undersample
+rf_under = randomForest(output~., data = undersampling, proximity=TRUE)
+print(rf_under)
+
+predict_rf_under = predict(rf_under, undersampling)
+confusionMatrix(predict_rf_under, undersampling$output)
 
 # Clustering
 d_dist = daisy(scaled_df, metric = "gower")
@@ -122,7 +192,18 @@ plot(hc, labels=FALSE)
 rect.hclust(hc, k=8, border="red")
 # choose k, number of clusters 
 cluster = cutree(hc, k=8)
-# add cluster to original data 
+
+
+## Clustering- Undersampling 
+d_dist_under = daisy(undersampling, metric = "gower")
+## hierarchical clustering 
+hc_undersampled = hclust(d_dist_under, method = "complete")
+## dendrogram Undersampling 
+plot(hc_undersampled, labels=FALSE)
+rect.hclust(hc_undersampled, k=8, border="red")
+## choose k, number of clusters 
+cluster = cutree(hc_undersampled, k= 8)
+
 
 # PCA
 pc1 = prcomp(scaled_df[,-14])
@@ -134,6 +215,10 @@ index2 = sort(sample(nrow(pca_scaled_df), nrow(pca_scaled_df) * 0.4))
 
 train2 = scaled_df[index2,]
 valid2 = scaled_df[-index2,]
+
+## Method for Undersampling PCA
+table(train2$output)
+under_pca<-ovun.sample(output~., data=train2, method='under', N = 114)$data
 
 # Logistic PCA
 mylogit2 <- glm(output~., data = train2, family = binomial)
@@ -161,6 +246,35 @@ acc3
 psuedo2 = 1 - (mylogit2$deviance/mylogit2$null.deviance)
 psuedo2
 
+## Logisitc PCA- Undersample
+under_log_pca<- glm(output~.,data=under_pca, family='binomial')
+summary(under_log_pca)
+
+##Predicting results
+predict_under_pca<-predict(under_log_pca, newdata= valid2 ,type='response')
+predict_under_pca
+
+##Converting them to alive and dead
+predict_under_response_pca<-ifelse(predict_under>=0.5, '1','0')
+
+##Dataframe
+dataframe_under_pca<- data.frame('Actual'=valid2$output,
+                             'Predicted'= predict_under_response_pca)
+
+View(dataframe_under_pca)
+
+##Creation of confusion matrix
+table_under_pca<-table('Actual'=dataframe_under_pca$Actual,
+                   'Predicted'= dataframe_under_pca$Predicted)
+table_under_pca
+
+##Creation of probability table
+prop_table_under_pca<-prop.table(table_under_pca)
+round(prop_table_under_pca*100,2)
+acc_under_pca<-44.51+35.16
+acc_under_pca
+
+
 # Classification PCA
 model2 = rpart(output~., data = train2, method = 'class')
 
@@ -185,12 +299,45 @@ acc4
 confusion_matrix = table(valid2$output, predict_test)
 confusion_matrix
 
+## Classification PCA -  Undersample 
+modunder_pca = rpart(output~., data = under_pca, method = 'class')
+
+rpart.plot(modunder_pca)
+
+predict_classunder_pca = predict(modunder_pca, valid2, type = "class")
+## Dataframe 
+under_class_df_pca = data.frame("Actual" = valid2$output,
+                            "Predicted" = predict_classunder_pca)
+View(predict_classunder_pca)
+
+tb_class_pca = table("Actual" = under_class_df_pca$Actual,
+                 "Predicted" = under_class_df_pca$Predicted)
+tb_class_pca
+
+prob_class_under_pca = round(prop.table(tb_class_pca) * 100, 2)
+prob_class_under_pca
+
+accuracy_under_class_pca = 42.86+36.81
+accuracy_under_class_pca
+
+confusion_matrix = table(valid2$output, predict_classunder_pca)
+confusion_matrix
+
+
 # Random Forests PCA
 rf2 = randomForest(output~., data = train2, proximity=TRUE) 
 print(rf2)
 
 p2 = predict(rf, train2)
 confusionMatrix(p2, train2$output)
+
+## Random Forest PCA -  Undersample 
+rf_under_pca = randomForest(output~., data = under_pca, proximity=TRUE)
+print(rf_under_pca)
+
+predict_rf_under_pca = predict(rf_under_pca, under_pca)
+confusionMatrix(predict_rf_under_pca, under_pca$output)
+## Accuracy of (.9682,1)
 
 # Clustering PCA
 d_dist2 = daisy(pca_scaled_df, metric = "gower")
@@ -202,5 +349,14 @@ rect.hclust(hc2, k=8, border="red")
 # choose k, number of clusters 
 cluster2 = cutree(hc2, k=8)
 
+## Clustering PCA - Undersample
+d_dist_under_pca = daisy(under_pca, metric = "gower")
+## hierarchical clustering
+hc_undersampled_pca = hclust(d_dist_under_pca, method = "complete")
+## dendrogram 
+plot(hc_undersampled_pca, labels=FALSE)
+rect.hclust(hc_undersampled_pca, k=8, border="red")
+## choose k, number of clusters 
+cluster = cutree(hc_undersampled_pca, k= 8)
 
 rm(list = ls())
